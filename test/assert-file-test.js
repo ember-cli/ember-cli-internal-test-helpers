100d1c197a0e2af02b5bdece252c668fad760240
Tobias Bieniek <tobias.bieniek@gmail.com>
2016-03-07 12:35:16 +0100

    test/assert-file: Add "isEmpty" tests

--- test/assert-file-test.js
+++ test/assert-file-test.js
@@ -11,4 +11,16 @@ describe('assert-file', function() {
       assertFile('test/fixtures/missing.txt');
     }).to.throw('AssertionError: expected test/fixtures/missing.txt to exist: expected false to equal true')
   });
+
+  describe('isEmpty', function() {
+    it('should pass if file is empty', function() {
+      assertFile('test/fixtures/empty.txt', { isEmpty: true });
+    });
+
+    it('should throw if file is not empty', function() {
+      expect(function() {
+        assertFile('test/fixtures/foo123.txt', { isEmpty: true });
+      }).to.throw('AssertionError: \n\nexpected test/fixtures/foo123.txt:\n\nfoo\n123\nfoo123foo\nbla\n\nto be empty.\n: expected \'foo\\n123\\nfoo123foo\\nbla\\n\' to equal \'\'')
+    });
+  });
 });
