f5623a9aef7435307db225f28c7d48d78e530d6d
Tobias Bieniek <tobias.bieniek@gmail.com>
2016-03-07 13:28:38 +0100

    test/assert-file: Add "contains" tests

--- test/assert-file-test.js
+++ test/assert-file-test.js
@@ -23,4 +23,46 @@ describe('assert-file', function() {
       }).to.throw('AssertionError: \n\nexpected test/fixtures/foo123.txt:\n\nfoo\n123\nfoo123foo\nbla\n\nto be empty.\n: expected \'foo\\n123\\nfoo123foo\\nbla\\n\' to equal \'\'')
     });
   });
+
+  describe('contains', function() {
+    it('should pass if file contains string', function() {
+      assertFile('test/fixtures/foo123.txt', { contains: 'foo' });
+    });
+
+    it('should throw if file does not contain string', function() {
+      expect(function() {
+        assertFile('test/fixtures/foo123.txt', { contains: 'bar' });
+      }).to.throw('EqualityError: expected: `test/fixtures/foo123.txt`')
+    });
+
+    it('should pass if file contains multiple strings', function() {
+      assertFile('test/fixtures/foo123.txt', { contains: ['foo', '123'] });
+    });
+
+    it('should throw if file does not contain one of multiple strings', function() {
+      expect(function() {
+        assertFile('test/fixtures/foo123.txt', { contains: ['foo', 'bar'] });
+      }).to.throw('EqualityError: expected: `test/fixtures/foo123.txt`')
+    });
+
+    it('should pass if file matches regex', function() {
+      assertFile('test/fixtures/foo123.txt', { contains: /fo+/ });
+    });
+
+    it('should throw if file does not match regex', function() {
+      expect(function() {
+        assertFile('test/fixtures/foo123.txt', { contains: /bar/ });
+      }).to.throw('EqualityError: expected: `test/fixtures/foo123.txt`')
+    });
+
+    it('should pass if file matches multiple regexes', function() {
+      assertFile('test/fixtures/foo123.txt', { contains: [/fo+/, /\d+/] });
+    });
+
+    it('should throw if file does not match one of multiple regexes', function() {
+      expect(function() {
+        assertFile('test/fixtures/foo123.txt', { contains: [/fo+/, /bar/] });
+      }).to.throw('EqualityError: expected: `test/fixtures/foo123.txt`')
+    });
+  });
 });
